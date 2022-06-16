7252ed10552c7b42b3bf589f570a6f82070e6163
ubuntushopdoteu <info@ubuntushop.eu>
2022-06-16 14:53:14 +0200

    Update firefoxfavorites.sh
    sometimes you need  --allow-downgrades to install firefox

--- firefoxfavorites.sh
+++ firefoxfavorites.sh
@@ -9,7 +9,7 @@ Pin-Priority: 1001
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' |  tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-apt-get -y  install  firefox
+apt-get -y  install  firefox --allow-downgrades 
 apt-get -y  install  gnome-shell-extension-manager
 #su - "$USER" -c "`gsettings set org.gnome.shell favorite-apps "['firefox.desktop', $(gsettings get org.gnome.shell favorite-apps | sed 's/^.//') "`"
 RUID=$(who | awk 'FNR == 1 {print $1}')
